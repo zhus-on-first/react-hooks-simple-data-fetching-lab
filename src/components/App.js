b61a0b15fe1db8b7d2067d91e90a45766a666450
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-28 20:49:49 -0700

    lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+  return <img src={dogImage} alt="A Random Dog" />;
+}
+
+export default App;
